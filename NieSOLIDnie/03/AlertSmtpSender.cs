ec05df398e48fc35a7af2e747b6f1966e8b3a08d
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2019-01-20 21:04:40 +0100

    change files into final blog post version

--- NieSOLIDnie/03/AlertSmtpSender.cs
+++ NieSOLIDnie/03/AlertSmtpSender.cs
@@ -8,24 +8,38 @@ namespace MessagingServer.Core.Mail
 {
     public class AlertSmtpSender : SmtpSender
     {
-        public AlertSmtpSender(ISmtpSettings smtpSettings, IMailTemplates mailTemplates)
-            : base(smtpSettings, mailTemplates) { }
+        private readonly string _machineId;
+        private readonly string _creationTime;
+        private readonly string _alertName;
+        private readonly string _alertInfo;
+        private readonly bool _usePlainText;
+        
 
-        public Task<bool> SendAlertMail(string recipientAddress, string recipientName,
-            bool usePlainText, string alertName, string machineId, DateTime creationTime, string alertInfo)
+        public string MachineId { get; set; }
+        public string CreationTime { get; set; }
+        public string AlertName { get; set; }
+        public string AlertInfo { get; set; }
+        public bool UsePlainText { get; set; }
+
+
+        public AlertSmtpSender(ISmtpSettings smtpSettings, IMailTemplates mailTemplates)
+            : base(smtpSettings, mailTemplates){}
+
+        public override Task<bool> SendMail(string toAddr, string toName, string mailSubject, string body, string anotherParameter)
         {
             var contentReplacements = new Dictionary<string, string>
             {
-                { "MACHINEID", machineId },
-                { "ALERTTIME", creationTime.ToString(CultureInfo.CurrentUICulture) },
-                { "ALERTNAME", alertName },
-                { "ALERTINFO", alertInfo }
+                {"MACHINEID", _machineId},
+                {"ALERTTIME", _creationTime.ToString(CultureInfo.CurrentUICulture)},
+                {"ALERTNAME", _alertName},
+                {"ALERTINFO", _alertInfo}
             };
 
-            var mailSubject = $"Alert: {alertName}, machine name: {machineId}";
-            var mailBody = MailTemplates.CreateMailContent("core", "alert", usePlainText, contentReplacements);
+            var subject = $"Alert: {_alertName}, machine name: {_machineId}. On subject: {mailSubject}";
+            var mailBody = MailTemplates.CreateMailContent("core", "alert", _usePlainText,
+                contentReplacements, body);
 
-            return SendMail(recipientAddress, recipientName, mailSubject, mailBody);
+            return base.SendMail(toAddr, toName, subject, mailBody);
         }
     }
 }
